b61ffdcec49df0590c83ea159d51217b8d66c1f6
ROHIT KUMAR PANDEY <41504034+rohitpandeydev@users.noreply.github.com>
2025-10-27 17:38:10 +0530

    Add Terraform configuration for AWS provider

--- 10_aws/terraform.tf
+++ 10_aws/terraform.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+  aws = {
+    source = "hashicorp/aws"
+    version = "~>6.0"
+    }
+  }
+  required_version = "~>1.1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::224231896688:role/terraform_build"
+  }
+}
 
